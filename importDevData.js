59c21c9254c8e159da913efba1d74313f82445c4
Ahmed Sayed <therealahme@gmail.com>
2023-11-11 05:46:32 +0200

    create importData file

---
+++ importDevData.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+require('dotenv').config({ path: './config.env' });
+const Product = require('./models/productModel');
+const connectDatabase = require('./utilities/dataBase');
+
+const productsData = JSON.parse(fs.readFileSync(`${__dirname}/DevData/products.js`, 'utf-8'))
+
+const start = async () => {
+  try {
+    await connectDatabase(); // Connect to the database
+
+    // First, delete existing data
+    await Product.deleteMany();
+
+    // Then, create new data
+    await Product.create(productsData, { validateBeforeSave: false });
+
+    console.log('Data deleted and loaded successfully👍');
+  } catch (error) {
+    console.log(error);
+  }
+
+  process.exit();
+};
+
+start();
