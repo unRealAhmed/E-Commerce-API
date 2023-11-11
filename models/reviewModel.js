04c71ec6d4d9d6ca64088dc0d20650d4ed1ba82d
Ahmed Sayed <therealahme@gmail.com>
2023-11-11 22:29:44 +0200

    create review model

---
+++ models/reviewModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: [1, 'Please provide a rating of at least 1.'],
+      max: [5, 'Please provide a rating no higher than 5.'],
+      required: [true, 'Please provide a rating.'],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide a title for your review.'],
+      maxlength: [100, 'Title cannot exceed 100 characters.'],
+    },
+    comment: {
+      type: String,
+      required: [true, 'Please provide your review text.'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+reviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review
