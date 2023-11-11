6e6cf5c2ee90dcce39b3ebe91857a586ce6ebf1a
Ahmed Sayed <therealahme@gmail.com>
2023-11-11 23:59:21 +0200

    fix create review controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,7 @@
 const Review = require("../models/reviewModel");
-const { getAll, createOne, getOne, updateOne, deleteOne } = require("./resourceController");
+const { getAll, getOne, updateOne, deleteOne } = require("./resourceController");
+const asyncHandler = require("../utilities/asyncHandler");
+const AppError = require("../utilities/appErrors");
 
 exports.setReviewUserIds = (req, res, next) => {
   if (!req.body.product) req.body.product = req.params.productId;
@@ -8,7 +10,38 @@ exports.setReviewUserIds = (req, res, next) => {
 };
 
 exports.getAllReviews = getAll(Review)
-exports.createReview = createOne(Review)
 exports.getReview = getOne(Review);
 exports.updateReview = updateOne(Review);
 exports.deleteReview = deleteOne(Review);
+
+
+exports.createReview = asyncHandler(async (req, res, next) => {
+  const productId = req.body.product;
+  const { user } = req;
+
+  // Check if the product ID is present in the request body
+  if (!req.body.product) {
+    throw new AppError('Product ID is required for creating a review.', 400);
+  }
+
+  // Check if the user has already reviewed the product
+  const existingReview = await Review.findOne({ product: productId, user: user._id });
+
+  if (existingReview) {
+    throw new AppError('You have already reviewed this product.', 400);
+  }
+
+  // If not, proceed with creating the review
+  const newReview = await Review.create({
+    ...req.body,
+    user: user._id,
+    product: productId,
+  });
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview,
+    },
+  });
+});
