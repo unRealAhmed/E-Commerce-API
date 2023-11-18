079125d61cbc232f9c6fcdbd48074e2b3b455e2b
Ahmed Sayed <therealahme@gmail.com>
2023-11-18 09:02:37 +0200

    fix the update and delete controllers

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,16 +1,11 @@
 const Review = require("../models/reviewModel");
-const { getAll, getOne, updateOne, deleteOne } = require("./resourceController");
+const { getAll, getOne } = require("./resourceController");
 const asyncHandler = require("../utilities/asyncHandler");
 const AppError = require("../utilities/appErrors");
 
-//
-
-const docType = 'review'
 
 exports.getAllReviews = getAll(Review)
-exports.getReview = getOne(Review, docType);
-exports.updateReview = updateOne(Review, docType);
-exports.deleteReview = deleteOne(Review, docType);
+exports.getReview = getOne(Review, 'review');
 
 exports.setReviewUserIds = (req, res, next) => {
   if (!req.body.product) req.body.product = req.params.productId;
@@ -47,4 +42,55 @@ exports.createReview = asyncHandler(async (req, res, next) => {
       review: newReview,
     },
   });
+});
+
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  const productId = req.body.product;
+  const { user } = req;
+
+  const existingReview = await Review.findOne({ product: productId, user: user._id });
+  if (existingReview) {
+    // 1. Find and update the document
+    const review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, // Return the updated document
+      runValidators: true, // Run validators on update
+    });
+
+    // 2. Handle the case when no document is found
+    if (!review) {
+      return next(new AppError(`No review found with that ID`, 404));
+    }
+
+    // 3. Send a success response with the updated data
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: review,
+      },
+    });
+  }
+});
+
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const productId = req.body.product;
+  const { user } = req;
+
+  const existingReview = await Review.findOne({ product: productId, user: user._id });
+  if (existingReview) {
+    // 1. Find and delete a document by its ID
+    const review = await Review.findByIdAndDelete(req.params.id);
+
+    // 2. If no document is found, handle it as an error
+    if (!review) {
+      return next(new AppError(`No review found with that ID`, 404));
+    }
+
+    // 3. Send a success response with no data (204 status code)
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } else {
+    return next(new AppError("you don't have permission to do this action", 400))
+  }
 });
